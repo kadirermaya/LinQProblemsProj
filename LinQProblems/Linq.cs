9f6914ab4cefc6d546ac3e028dc07e8473bf8627
kadirermaya <kadir.ermaya@gmail.com>
2020-07-07 12:55:46 -0700

    Created a Linq Class and solved first problem

---
+++ LinQProblems/Linq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQProblems
+{
+    public class Linq
+    {
+        //  Using LINQ, write a function that returns all words that contain the substring “th” from a list.
+        //	List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", “mathematics” };
+
+        public IEnumerable<string> IfContains(string str)
+        {
+            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var contains = words.Where(word => word.Contains(str)).Select(word => word);
+            return contains;
+        }
+
+    }
+}
