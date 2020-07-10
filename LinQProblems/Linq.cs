847ca733a15dccbefb2ab4351ca4ae440973563d
kadirermaya <kadir.ermaya@gmail.com>
2020-07-10 09:27:05 -0700

    update

--- LinQProblems/Linq.cs
+++ LinQProblems/Linq.cs
@@ -29,10 +29,22 @@ namespace LinQProblems
             return namesWithoutDuplicated;
         }
 
+        //  Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student.
+        //  The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest 
+        //  grade from each string, averages the rest of the grades from that string, then averages the averages.
+        //  List<string> classGrades = new List<string>()
+        //  {
+        //  "80,100,92,89,65",
+        //  "93,81,78,84,69",
+        //  "73,88,83,99,64",
+        //  "98,100,66,74,55"
+        //  };
+        //  Expected output: 86.125
+
         
-	
 
-	}
 
-    
+    }
+
+
 }
